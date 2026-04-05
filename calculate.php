27308a451b77266c3d12682a7dc94646aa924809
asalagaid036-coder <asalagaid036@gmail.com>
2026-04-05 21:08:51 +0100

    Add GPA calculation and JSON response in calculate.php

---
+++ calculate.php
@@ -0,0 +1,62 @@
+<?php
+header('Content-Type: application/json');
+
+if (isset($_POST['course'], $_POST['credits'], $_POST['grade'])) {
+
+    $courses = $_POST['course'];
+    $credits = $_POST['credits'];
+    $grades = $_POST['grade'];
+
+    $totalPoints = 0;
+    $totalCredits = 0;
+
+    $tableHtml = '<table class="table table-bordered mt-3">';
+    $tableHtml .= '<tr><th>Course</th><th>Credits</th><th>Grade</th><th>Points</th></tr>';
+
+    for ($i = 0; $i < count($courses); $i++) {
+
+        $course = htmlspecialchars($courses[$i]);
+        $cr = floatval($credits[$i]);
+        $g = floatval($grades[$i]);
+
+        if ($cr <= 0) continue;
+
+        $pts = $cr * $g;
+
+        $totalPoints += $pts;
+        $totalCredits += $cr;
+
+        $tableHtml .= "<tr>
+        <td>$course</td>
+        <td>$cr</td>
+        <td>$g</td>
+        <td>$pts</td>
+        </tr>";
+    }
+
+    $tableHtml .= '</table>';
+
+    if ($totalCredits > 0) {
+
+        $gpa = $totalPoints / $totalCredits;
+
+        if ($gpa >= 3.7) $msg = "Distinction";
+        elseif ($gpa >= 3.0) $msg = "Merit";
+        elseif ($gpa >= 2.0) $msg = "Pass";
+        else $msg = "Fail";
+
+        echo json_encode([
+            'success' => true,
+            'gpa' => $gpa,
+            'message' => "Your GPA is " . number_format($gpa, 2) . " ($msg)",
+            'tableHtml' => $tableHtml
+        ]);
+
+    } else {
+        echo json_encode(['success' => false, 'message' => 'No valid data']);
+    }
+
+} else {
+    echo json_encode(['success' => false, 'message' => 'No data received']);
+}
+?>
